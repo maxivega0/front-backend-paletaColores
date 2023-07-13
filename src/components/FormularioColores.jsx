ebc9ca596fdd369ed1e4d66e4683da7c6676660c
maxivega0 <maximilianovega600@gmail.com>
2023-07-13 03:59:39 -0300

    implementacion funciones de borrado y editar de colores

--- src/components/FormularioColores.jsx
+++ src/components/FormularioColores.jsx
@@ -1,53 +1,79 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Form, Button, Container } from "react-bootstrap";
 import ItemColor from "./ItemColor";
-// import { useForm } from "react-hook-form";
-// import Swal from "sweetalert2";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { crearColor } from "../helpers/queries";
 
 const FormularioColor = () => {
-  const coloresLocalStorage = JSON.parse(localStorage.getItem('listaColores')) || [];
-  const [color, setColor] = useState("");
-  const [colores, setColores] = useState(coloresLocalStorage)
-  
-  useEffect(() => {
-    localStorage.setItem('listaColores', JSON.stringify(colores))
-  }, [colores])
+  const [cargar, setCargar] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setColores([...colores, color]);
-    setColor("");
-  }
-  
-  const borrarColor = (nombreColor) =>{
-    let copiaColores = colores.filter((itemColor) => itemColor !== nombreColor);
-    setColores(copiaColores);
-  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (nuevoColor) => {
+    console.log(nuevoColor);
+    crearColor(nuevoColor).then((respuesta) => {
+      if (respuesta.status === 201) {
+        Swal.fire(
+          "Producto creado correctamente!",
+          `El producto ${nuevoColor.color} fue creado correctamente`,
+          "success"
+        );
+        reset();
+        setCargar(true);
+      } else {
+        Swal.fire(
+          "Ocurrio un error!",
+          `El producto ${nuevoColor.color} no pudo ser creado`,
+          "error"
+        );
+      }
+    });
+    reset();
+  };
+
+  // const borrarColor = (nombreColor) =>{
+  //   let copiaColores = colores.filter((itemColor) => itemColor !== nombreColor);
+  //   setColores(copiaColores);
+  // }
 
   return (
     <>
-      <Form onSubmit={handleSubmit} className="bloqueFormulario">
-        <Form.Group className="mb-3 d-flex flex-column" controlId="tarea">
+      <Form onSubmit={handleSubmit(onSubmit)} className="bloqueFormulario">
+        <Form.Group className="mb-3 d-flex flex-column" controlId="color">
+          <Form.Label>Color*</Form.Label>
           <Form.Control
             type="text"
-            placeholder="Ingrese una color"
-            onChange={(e) => setColor(e.target.value)}
-            value={color}
+            placeholder="Ej: alguna descripcion"
+            {...register("color", {
+              required: "El nombre del producto es obligatorio",
+              minLength: {
+                value: 2,
+                message: "La cantidad minima de caracteres es de 2 digitos",
+              },
+              maxLength: {
+                value: 150,
+                message: "La cantidad maxima de caracteres es de 150 digitos",
+              },
+            })}
           />
+          <Form.Text className="text-danger">{errors.color?.message}</Form.Text>
         </Form.Group>
-
-          <Container className="my-3 text-center">
+        <Container className="my-3 text-center">
           <Button variant="primary" type="submit">
             Guardar
           </Button>
-          </Container>
-  
+        </Container>
       </Form>
 
       <Container className="d-flex row justify-content-center">
-        <ItemColor colores={colores} borrarColor={borrarColor}></ItemColor>
+        <ItemColor cargar={cargar} setCargar={setCargar}></ItemColor>
       </Container>
-      
     </>
   );
 };
--- src/components/ItemColor.jsx
+++ src/components/ItemColor.jsx
@@ -1,28 +1,193 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-// import Card from 'react-bootstrap/Card';
+import { useState } from "react";
+import { useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Swal from "sweetalert2";
+import {
+  editarColor,
+  eliminarColor,
+  obtenerListaColores,
+} from "../helpers/queries";
+import Modal from "react-bootstrap/Modal";
+import { Container, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
-const ItemColor = ({colores, borrarColor }) => {
-    return (    
+// eslint-disable-next-line react/prop-types
+const ItemColor = ({ cargar, setCargar }) => {
+  const [colorId, setColorId] = useState("");
+
+  const [colores, setColores] = useState([]);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  useEffect(() => {
+    obtenerListaColores().then((respuesta) => {
+      //todo: preguntar si la respuesta tiene
+      if (respuesta) {
+        setColores(respuesta);
+        setCargar(false);
+      } else {
+        Swal.fire(
+          "Error",
+          "Intente realizar esta operación en unos minutos",
+          "error"
+        );
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cargar]);
+
+  const handleEliminarColor = (color) => {
+    Swal.fire({
+      title: "Esta seguro de borrar la siguiente tarea?",
+      text: "El siguiente cambio no podra ser revertido",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, quiero borrar!",
+      cancelButtonText: "Cancelar",
+    }).then((resultado) => {
+      if (resultado.isConfirmed) {
+        eliminarColor(color._id).then((respuesta) => {
+          if (respuesta.status === 200) {
+            obtenerListaColores().then((respuesta) => {
+              if (respuesta) {
+                setColores(respuesta);
+              } else {
+                Swal.fire(
+                  "Error",
+                  "Intente realizar esta operacion en unos minutos",
+                  "error"
+                );
+              }
+            });
+            Swal.fire("Borrado!", "El color fue borrado.", "success");
+          } else {
+            Swal.fire({
+              title: "Lo siento!",
+              text: "El color no pudo ser eliminado.",
+              icon: "error",
+              confirmButtonColor: "#fa8072",
+            });
+          }
+        });
+      }
+    });
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (colorEditado) => {
+    console.log(colorEditado);
+    editarColor(colorEditado, colorId).then((respuesta) => {
+      if (respuesta.status === 200) {
+        Swal.fire(
+          "Producto editado correctamente!",
+          `El producto ${colorEditado.color} sufrió cambios`,
+          "success"
+        );
+        reset();
+        setCargar(true);
+      } else {
+        Swal.fire(
+          "Ocurrio un error!",
+          `El producto ${colorEditado.color} no pudo ser editado`,
+          "error"
+        );
+      }
+    });
+    // then implica lo siguiente: yo ejecutare una funcion, una vez que se ejecute iniciar sesion, espera que se ejecute y entonces, realiza lo siguiente
+    // respuesta es una variable inventada que va a contener el return de "inciarSesion"
+    Swal.fire("Producto editado correctamente!", "", "success");
+    reset();
+  };
+
+  return (
     <>
       {colores.map((color) => {
         return (
-          <Card className="mx-2 my-2 col-12 col-md-3" style={{ width: "18rem" }} key={color}>
-            <Card.Body style={{ background: color, height: "10rem" }}></Card.Body>
+          <Card
+            className="mx-2 my-2 col-12 col-md-3"
+            style={{ width: "18rem" }}
+            key={color._id}
+          >
+            <Card.Body
+              style={{ background: color.color, height: "10rem" }}
+            ></Card.Body>
             <Card.Body className="d-flex justify-content-between">
-              <Card.Title>{color}</Card.Title>
+              <Card.Title>{color.color}</Card.Title>
             </Card.Body>
-            <Card.Footer className='justify-content-'>
-              <Button variant="danger mx-4" onClick={() => borrarColor(color)}>Borrar</Button>
-              <Button variant="warning" onClick={() => borrarColor(color)}>Editar</Button>
+            <Card.Footer className="justify-content-">
+              <Button
+                variant="danger mx-4"
+                onClick={() => handleEliminarColor(color)}
+              >
+                Borrar
+              </Button>
+              <Button
+                variant="warning"
+                onClick={() => {
+                  handleShow();
+                  setColorId(color._id);
+                  console.log(colorId);
+                }}
+              >
+                Editar
+              </Button>
             </Card.Footer>
           </Card>
         );
       })}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Cambiar color</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {" "}
+          <Form onSubmit={handleSubmit(onSubmit)} className="bloqueFormulario">
+            <Form.Group className="mb-3 d-flex flex-column" controlId="color">
+              <Form.Label>Color*</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Escriba el nuevo color"
+                {...register("color", {
+                  required: "El nombre del producto es obligatorio",
+                  minLength: {
+                    value: 2,
+                    message: "La cantidad minima de caracteres es de 2 digitos",
+                  },
+                  maxLength: {
+                    value: 150,
+                    message:
+                      "La cantidad maxima de caracteres es de 150 digitos",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.color?.message}
+              </Form.Text>
+            </Form.Group>
+            <Container className="my-3 text-center">
+              <Button variant="secondary" onClick={handleClose}>
+                Cerrar
+              </Button>
+              <Button variant="primary" type="submit">
+                Guardar cambios
+              </Button>
+            </Container>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </>
-      
-    );
-    
+  );
 };
 
 export default ItemColor;
